50541a886cb39add2b673eb626e351ee76d0c82e
Frizlab <fload@me.com>
2024-02-22 14:15:22 +0100

    Call the common init in iOS SVGView

--- Example/IJSVGExample for iOS/SVGView.swift
+++ Example/IJSVGExample for iOS/SVGView.swift
@@ -19,10 +19,12 @@ final class SVGView : UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        commonInit()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        commonInit()
     }
     
     private func commonInit() {
